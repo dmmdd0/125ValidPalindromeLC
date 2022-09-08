d5036499b41cb1913a89b372eee0b9bcc049f8b4
dimm <witty.sock3113@fastmail.com>
2022-09-08 07:53:07 +0200

    make slice with lower case only

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
 	palindrom := ""
+	palindrom = "A man, a plan, a canal: Panama"
 	fmt.Println(isPalindrome(palindrom))
 
 }
 
 func isPalindrome(s string) bool {
-	
+	// remove spases
+	//res := strings.Join(strings.Fields(s), "")
+
+	// remove all no letter & lower case
+	res := make([]string, 0, len(s))
+	for _, v := range s {
+		switch {
+		case v >= 90 && v <= 122:
+			res = append(res, string(v))
+		case v >= 65 && v <= 90:
+			res = append(res, string(v+32))
+		}
+	}
+
+	return false
 }
